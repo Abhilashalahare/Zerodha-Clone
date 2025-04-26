644fc3a4c7242243fc2bb5cb27398ce219032dd1
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-04-26 21:56:41 +0530

    Update vite.config.js

--- vite-frontend/vite.config.js
+++ vite-frontend/vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-=======
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-  ],
->>>>>>> home-component
-})
+  ]
+});
