c021f03fa074266079c220433500aca156439ab8
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-04-26 21:55:40 +0530

    Update main.jsx

--- vite-frontend/src/main.jsx
+++ vite-frontend/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -27,4 +15,4 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
 );
->>>>>>> home-component
+
